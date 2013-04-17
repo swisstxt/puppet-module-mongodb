74863160a455bab1fa058cd105a5b6f127ea87d0
niwo <nik.wolfgramm@swisstxt.ch>
2013-04-17 12:05:40 +0200

    use our own yumrepo wrapper for now

--- manifests/repos/yum.pp
+++ manifests/repos/yum.pp
@@ -8,7 +8,7 @@
 #
 class mongodb::repos::yum {
 
-	yumrepo { 'mongodb_yum_repo':
+	yum::repo { 'mongodb_yum_repo':
 		descr         => '10gen MongoDB Repo',
 		baseurl       => 'http://downloads-distro.mongodb.org/repo/redhat/os/$basearch',
 		enabled       => 1,
